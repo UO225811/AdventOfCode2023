8f78103d0f1ca8cd8dea03f0527fc2fca0c2e0ea
Sergio <60402138+UO225811@users.noreply.github.com>
2023-12-07 13:36:53 +0100

    Now regex works depending on begin/end occurrences

--- AdventDay1/src/advent2/CounterNumbersAndWordsMeaningNumbers.java
+++ AdventDay1/src/advent2/CounterNumbersAndWordsMeaningNumbers.java
@@ -14,17 +14,48 @@ public class CounterNumbersAndWordsMeaningNumbers {
 	public static void main(String[] args) {
 		String path = "text.txt";
 		int result = 0;
-
-		Map<String, Integer> wordToNumber = new HashMap<>();
-		wordToNumber.put("one", 1);
-		wordToNumber.put("two", 2);
-		wordToNumber.put("three", 3);
-		wordToNumber.put("four", 4);
-		wordToNumber.put("five", 5);
-		wordToNumber.put("six", 6);
-		wordToNumber.put("seven", 7);
-		wordToNumber.put("eight", 8);
-		wordToNumber.put("nine", 9);
+		
+		//Map for searching the previous occurrences
+		Map<String, Integer> wordToNumber1 = new HashMap<>();
+		wordToNumber1.put("one", 1);
+		wordToNumber1.put("two", 2);
+		wordToNumber1.put("three", 3);
+		wordToNumber1.put("four", 4);
+		wordToNumber1.put("five", 5);
+		wordToNumber1.put("six", 6);
+		wordToNumber1.put("seven", 7);
+		wordToNumber1.put("eight", 8);
+		wordToNumber1.put("nine", 9);
+		
+		wordToNumber1.put("oneight", 1);
+		wordToNumber1.put("twone", 2);
+		wordToNumber1.put("threeight", 3);
+		wordToNumber1.put("fiveight", 5);
+		wordToNumber1.put("nineight", 9);
+		wordToNumber1.put("eighthree", 8);
+		wordToNumber1.put("eightwo", 8);
+		wordToNumber1.put("sevenine", 7);
+		
+		//Map for searching after the first occurrence was found
+		Map<String, Integer> wordToNumber2 = new HashMap<>();
+		wordToNumber2.put("one", 1);
+		wordToNumber2.put("two", 2);
+		wordToNumber2.put("three", 3);
+		wordToNumber2.put("four", 4);
+		wordToNumber2.put("five", 5);
+		wordToNumber2.put("six", 6);
+		wordToNumber2.put("seven", 7);
+		wordToNumber2.put("eight", 8);
+		wordToNumber2.put("nine", 9);
+		
+		wordToNumber2.put("oneight", 8);
+		wordToNumber2.put("twone", 1);
+		wordToNumber2.put("threeight", 8);
+		wordToNumber2.put("fiveight", 8);
+		wordToNumber2.put("nineight", 8);
+		wordToNumber2.put("eighthree", 3);
+		wordToNumber2.put("eightwo", 2);
+		wordToNumber2.put("sevenine", 9);
 
 		try (FileInputStream file = new FileInputStream(path);
 				InputStreamReader input = new InputStreamReader(file);
@@ -34,26 +65,26 @@ public class CounterNumbersAndWordsMeaningNumbers {
 			String line;
 			while ((line = bufferedReader.readLine()) != null) {
 				// Pattern to find all the digits or number in words inside of the string
-				Pattern pattern = Pattern.compile("\\d|(?:one|two|three|four|five|six|seven|eight|nine)");
+				Pattern pattern = Pattern.compile("(\\d)|(twone|oneight|threeight|fiveight|nineight|eighthree|eightwo|nineight|sevenine|one|two|three|four|five|six|seven|eight|nine)");
 				Matcher matcher = pattern.matcher(line);
 				int firstDigit = -1, lastDigit = -1;
 
 				// Print all the digits found (included words)
 				while (matcher.find()) {
 					String theMatch = matcher.group();
-
-					if (theMatch.matches("\\d+")) {
+					if (theMatch.matches("\\d")) {
 						int digit = Integer.parseInt(theMatch);
 						if (firstDigit == -1) {
 							firstDigit = digit;
-						}							
+						}
 						lastDigit = digit;
 					} else {
-						int wordNumber = wordToNumber.get(theMatch);
+						int wordNumber = wordToNumber1.get(theMatch);
 
 						if (firstDigit == -1) {
+							wordNumber = wordToNumber1.get(theMatch);
 							firstDigit = wordNumber;
-						}
+						} else wordNumber = wordToNumber2.get(theMatch);
 						lastDigit = wordNumber;
 					}
 				}
